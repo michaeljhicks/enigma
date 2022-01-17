8f058c4af1277b72c8516d70ac4a8c454d504bd8
Michael Hicks <hicks.michael04@gmail.com>
2022-01-17 11:35:32 -0700

    test: add has attributes

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,4 +1,12 @@
+require_relative 'key_generator'
+require_relative 'offset_generator'
+
 class ShiftGenerator
-  
+attr_reader :key_generator, :offset_generator
+
+  def initialize
+    @key_generator = KeyGenerator.new
+    @offset_generator = OffsetGenerator.new
+  end
 
 end
--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -4,13 +4,21 @@ require './lib/shift_generator'
  RSpec.describe ShiftGenerator do
  	before(:each) do
  		@shift_generator = ShiftGenerator.new
+    @key_generator = KeyGenerator.new
+    @offset_generator = OffsetGenerator.new
  	end
 
  	it 'exists' do
  		expect(@shift_generator).to be_a(ShiftGenerator)
  	end
 
- 	xit 'has attributes' do
- 		expect(@shift_generator.attribute).to eq(attribute)
- 	end
+  it "has attributes" do
+    expect(@shift_generator.key_generator).to be_a(KeyGenerator)
+    expect(@shift_generator.offset_generator).to be_a(OffsetGenerator)
+  end
+
+  xit "text" do
+
+  end
+
  end
