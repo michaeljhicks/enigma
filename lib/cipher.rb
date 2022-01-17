54fe21c51eb541a89af8bef1226cba9e3b33509a
Michael Hicks <hicks.michael04@gmail.com>
2022-01-17 12:36:26 -0700

    test: add has attributes

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,6 +1,18 @@
 require_relative './modules/calculable'
+require_relative 'shift_generator'
 
 class Cipher
   include Calculable
+  attr_reader :default_key,
+              :default_date,
+              :alphabet,
+              :shift_generator
+
+  def initialize
+    @default_key = calculate_random_key
+    @default_date = calculate_date
+    @alphabet = ("a".."z").to_a << " "
+    @shift_generator = ShiftGenerator.new
+  end
 
 end
--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -10,7 +10,14 @@ require './lib/cipher'
  		expect(@cipher).to be_a(Cipher)
  	end
 
- 	# it 'has attributes' do
- 	# 	expect(@cipher.attribute).to eq(attribute)
- 	# end
+ 	it 'has attributes' do
+    expected = ["a", "b", "c", "d", "e", "f",
+      "g", "h", "i", "j", "k", "l", "m", "n",
+      "o", "p", "q", "r", "s", "t", "u", "v",
+      "w", "x", "y", "z", " "]
+ 		expect(@cipher.alphabet).to eq(expected)
+ 		expect(@cipher.default_key).to be_a(String)
+    expect(@cipher.default_key.length).to eq(5)
+ 		expect(@cipher.default_date).to eq("170122")
+ 	end
  end
